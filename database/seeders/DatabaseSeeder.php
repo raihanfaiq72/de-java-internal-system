7fceccfdb11fbf95b385a1cbb813fc4c86cd42bf
IQ72 <faiq.fhr03@gmail.com>
2026-01-04 21:51:19 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ChartOfAccountSeeder;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\SuperAdminSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +20,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call(PermissionSeeder::class);
+        $this->call(SuperAdminSeeder::class);
+        $this->call(ChartOfAccountSeeder::class);
     }
 }
