ed5d52b2a80f0808059c5367091e6a332b45a4e4
IQ72 <faiq.fhr03@gmail.com>
2026-01-05 18:28:29 +0700

    Update _sidebar.blade.php

--- resources/views/Layout/_sidebar.blade.php
+++ resources/views/Layout/_sidebar.blade.php
@@ -3,88 +3,95 @@
         <div class="d-flex align-items-start flex-column w-100">
             <!-- Navigation -->
             <ul class="navbar-nav mb-auto w-100">
+                <!-- MAIN -->
                 <li class="menu-label mt-2">
                     <span>Main</span>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link" href="index.html">
-                        <i class="iconoir-report-columns menu-icon"></i>
+                        <i class="iconoir-dashboard menu-icon"></i>
                         <span>Dashboard</span>
                         <span class="badge text-bg-info ms-auto">New</span>
                     </a>
                 </li>
-                <!--end nav-item-->
+
+                <!-- MASTER -->
                 <li class="nav-item">
-                    <a class="nav-link" href="#sidebarTransactions" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarTransactions">
-                        <i class="iconoir-task-list menu-icon"></i>
+                    <a class="nav-link" href="#sidebarMaster" data-bs-toggle="collapse" role="button"
+                        aria-expanded="false" aria-controls="sidebarMaster">
+                        <i class="iconoir-database menu-icon"></i>
                         <span>Master</span>
                     </a>
-                    <div class="collapse " id="sidebarTransactions">
+                    <div class="collapse" id="sidebarMaster">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link" href="transactions.html">Piutang</a>
+                                <a class="nav-link" href="transactions.html">
+                                    <i class="iconoir-wallet me-2"></i> Piutang
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="new-transaction.html">Admin</a>
+                                <a class="nav-link" href="new-transaction.html">
+                                    <i class="iconoir-user-circle me-2"></i> Admin
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="new-transaction.html">Sales</a>
+                                <a class="nav-link" href="new-transaction.html">
+                                    <i class="iconoir-handshake me-2"></i> Sales
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="new-transaction.html">Barang</a>
+                                <a class="nav-link" href="new-transaction.html">
+                                    <i class="iconoir-box me-2"></i> Barang
+                                </a>
                             </li>
                         </ul>
-                        <!--end nav-->
                     </div>
-                    <!--end startbarTables-->
                 </li>
+
+                <!-- TRANSAKSI -->
                 <li class="menu-label mt-2">
                     <span>Transaksi</span>
                 </li>
-                <!--end nav-item-->
+
                 <li class="nav-item">
                     <a class="nav-link" href="cards.html">
-                        <i class="iconoir-credit-cards menu-icon"></i>
+                        <i class="iconoir-cart menu-icon"></i>
                         <span>Pembelian</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="cards.html">
-                        <i class="iconoir-credit-cards menu-icon"></i>
+                        <i class="iconoir-home-sale menu-icon"></i>
                         <span>Penjualan</span>
                     </a>
                 </li>
 
+                <!-- REPORT -->
                 <li class="menu-label mt-2">
                     <span>Report</span>
                 </li>
-                <!--end nav-item-->
+
                 <li class="nav-item">
                     <a class="nav-link" href="cards.html">
-                        <i class="iconoir-credit-cards menu-icon"></i>
+                        <i class="iconoir-reports menu-icon"></i>
                         <span>Laporan Pembelian</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="cards.html">
-                        <i class="iconoir-credit-cards menu-icon"></i>
+                        <i class="iconoir-reports menu-icon"></i>
                         <span>Laporan Penjualan</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="cards.html">
-                        <i class="iconoir-credit-cards menu-icon"></i>
+                        <i class="iconoir-archive menu-icon"></i>
                         <span>Laporan Stock</span>
                     </a>
                 </li>
-                
-                <!--end nav-item-->
-                
-                <!--end nav-item-->
+
             </ul>
         </div>
     </div>
-    <!--end startbar-collapse-->
 </div>
