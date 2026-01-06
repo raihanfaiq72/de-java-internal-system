5dce19471efaefb3e4de4b862fac11a78ca2e504
IQ72 <faiq.fhr03@gmail.com>
2026-01-06 20:16:06 +0700

    feat: (errors) layout code

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('code', '404')
+@section('title', 'Oops! Halaman Tidak Ditemukan')
+@section('subtitle', 'Page Not Found')
+@section('message', 'URL yang kamu akses tidak tersedia')
