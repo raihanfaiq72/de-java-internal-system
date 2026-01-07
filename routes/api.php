1ed9297dd539ce35d4f613e11d37b0a5868d98b9
Nelv <nelvari2018rplrus2@gmail.com>
2026-01-07 13:40:31 +0700

    fix(): name api root

--- routes/api.php
+++ routes/api.php
@@ -17,123 +17,152 @@ use App\Http\Controllers\Api\UnitCategoryController;
 use App\Http\Controllers\Api\UnitController;
 use App\Http\Controllers\Api\UserController;
 
-Route::prefix('mitra-api')->group(function() {
-    Route::get('/', [MitraController::class, 'index']);
-    Route::post('/', [MitraController::class, 'store']);
-    Route::get('/{id}', [MitraController::class, 'show']);
-    Route::put('/{id}', [MitraController::class, 'update']);
-    Route::delete('/{id}', [MitraController::class, 'destroy']);
-    Route::get('/search/{value}', [MitraController::class, 'search']);
-});
+Route::prefix('mitra-api')
+    ->name('mitra-api.')
+    ->group(function () {
+        Route::get('/', [MitraController::class, 'index'])->name('index');
+        Route::post('/', [MitraController::class, 'store'])->name('store');
+        Route::get('/{id}', [MitraController::class, 'show'])->name('show');
+        Route::put('/{id}', [MitraController::class, 'update'])->name('update');
+        Route::delete('/{id}', [MitraController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [MitraController::class, 'search'])->name('search');
+    });
 
-Route::prefix('unit-categories-api')->group(function () {
-    Route::get('/', [UnitCategoryController::class, 'index']);
-    Route::post('/', [UnitCategoryController::class, 'store']);
-    Route::get('/{id}', [UnitCategoryController::class, 'show']);
-    Route::put('/{id}', [UnitCategoryController::class, 'update']);
-    Route::delete('/{id}', [UnitCategoryController::class, 'destroy']);
-    Route::get('/search/{value}', [UnitCategoryController::class, 'search']);
-});
+Route::prefix('unit-category-api')
+    ->name('unit-category-api.')
+    ->group(function () {
+        Route::get('/', [UnitCategoryController::class, 'index'])->name('index');
+        Route::post('/', [UnitCategoryController::class, 'store'])->name('store');
+        Route::get('/{id}', [UnitCategoryController::class, 'show'])->name('show');
+        Route::put('/{id}', [UnitCategoryController::class, 'update'])->name('update');
+        Route::delete('/{id}', [UnitCategoryController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [UnitCategoryController::class, 'search'])->name('search');
+    });
 
-Route::prefix('unit-api')->group(function () {
-    Route::get('/', [UnitController::class, 'index']);
-    Route::post('/', [UnitController::class, 'store']);
-    Route::get('/{id}', [UnitController::class, 'show']);
-    Route::put('/{id}', [UnitController::class, 'update']);
-    Route::delete('/{id}', [UnitController::class, 'destroy']);
-    Route::get('/search/{value}', [UnitController::class, 'search']);
-});
+Route::prefix('unit-api')
+    ->name('unit-api.')
+    ->group(function () {
+        Route::get('/', [UnitController::class, 'index'])->name('index');
+        Route::post('/', [UnitController::class, 'store'])->name('store');
+        Route::get('/{id}', [UnitController::class, 'show'])->name('show');
+        Route::put('/{id}', [UnitController::class, 'update'])->name('update');
+        Route::delete('/{id}', [UnitController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [UnitController::class, 'search'])->name('search');
+    });
 
-Route::prefix('product-categories-api')->group(function () {
-    Route::get('/', [ProductCategoryController::class, 'index']);
-    Route::post('/', [ProductCategoryController::class, 'store']);
-    Route::get('/{id}', [ProductCategoryController::class, 'show']);
-    Route::put('/{id}', [ProductCategoryController::class, 'update']);
-    Route::delete('/{id}', [ProductCategoryController::class, 'destroy']);
-    Route::get('/search/{value}', [ProductCategoryController::class, 'search']);
-});
+Route::prefix('product-category-api')
+    ->name('product-category-api.')
+    ->group(function () {
+        Route::get('/', [ProductCategoryController::class, 'index'])->name('index');
+        Route::post('/', [ProductCategoryController::class, 'store'])->name('store');
+        Route::get('/{id}', [ProductCategoryController::class, 'show'])->name('show');
+        Route::put('/{id}', [ProductCategoryController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ProductCategoryController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [ProductCategoryController::class, 'search'])->name('search');
+    });
 
-Route::prefix('product-api')->group(function () {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::post('/', [ProductController::class, 'store']);
-    Route::get('/{id}', [ProductController::class, 'show']);
-    Route::put('/{id}', [ProductController::class, 'update']);
-    Route::delete('/{id}', [ProductController::class, 'destroy']);
-    Route::get('/search/{value}', [ProductController::class, 'search']);
-});
+Route::prefix('product-api')
+    ->name('product-api.')
+    ->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::get('/{id}', [ProductController::class, 'show'])->name('show');
+        Route::put('/{id}', [ProductController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [ProductController::class, 'search'])->name('search');
+    });
 
-Route::get('/product-next-sku', [ProductController::class, 'nextSku']);
+Route::get('/product-next-sku-api', [ProductController::class, 'nextSku'])
+    ->name('product.next-sku-api');
 
-Route::prefix('tax-api')->group(function () {
-    Route::get('/', [TaxController::class, 'index']);
-    Route::post('/', [TaxController::class, 'store']);
-    Route::get('/{id}', [TaxController::class, 'show']);
-    Route::put('/{id}', [TaxController::class, 'update']);
-    Route::delete('/{id}', [TaxController::class, 'destroy']);
-    Route::get('/search/{value}', [TaxController::class, 'search']);
-});
+Route::prefix('tax-api')
+    ->name('tax-api.')
+    ->group(function () {
+        Route::get('/', [TaxController::class, 'index'])->name('index');
+        Route::post('/', [TaxController::class, 'store'])->name('store');
+        Route::get('/{id}', [TaxController::class, 'show'])->name('show');
+        Route::put('/{id}', [TaxController::class, 'update'])->name('update');
+        Route::delete('/{id}', [TaxController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [TaxController::class, 'search'])->name('search');
+    });
 
-Route::post('/invoice-create-api', [InvoiceController::class, 'createFullInvoice']);
 
-Route::prefix('invoice-api')->group(function () {
-    Route::get('/', [InvoiceController::class, 'index']);
-    Route::post('/', [InvoiceController::class, 'store']);
-    Route::get('/{id}', [InvoiceController::class, 'show']);
-    Route::put('/{id}', [InvoiceController::class, 'update']);
-    Route::delete('/{id}', [InvoiceController::class, 'destroy']);
-    Route::get('/search/{value}', [InvoiceController::class, 'search']);
-});
+Route::post('/invoice-create-api', [InvoiceController::class, 'createFullInvoice'])
+    ->name('invoice.create-full-api');
 
-Route::prefix('invoice-item-api')->group(function () {
-    Route::get('/', [InvoiceItemController::class, 'index']);
-    Route::post('/', [InvoiceItemController::class, 'store']);
-    Route::get('/{id}', [InvoiceItemController::class, 'show']);
-    Route::put('/{id}', [InvoiceItemController::class, 'update']);
-    Route::delete('/{id}', [InvoiceItemController::class, 'destroy']);
-    Route::get('/search/{value}', [InvoiceItemController::class, 'search']);
-});
+Route::prefix('invoice-api')
+    ->name('invoice-api.')
+    ->group(function () {
+        Route::get('/', [InvoiceController::class, 'index'])->name('index');
+        Route::post('/', [InvoiceController::class, 'store'])->name('store');
+        Route::get('/{id}', [InvoiceController::class, 'show'])->name('show');
+        Route::put('/{id}', [InvoiceController::class, 'update'])->name('update');
+        Route::delete('/{id}', [InvoiceController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [InvoiceController::class, 'search'])->name('search');
+    });
 
-Route::prefix('invoice-item-tax-api')->group(function () {
-    Route::get('/', [InvoiceItemTaxController::class, 'index']);
-    Route::post('/', [InvoiceItemTaxController::class, 'store']);
-    Route::get('/{id}', [InvoiceItemTaxController::class, 'show']);
-    Route::put('/{id}', [InvoiceItemTaxController::class, 'update']);
-    Route::delete('/{id}', [InvoiceItemTaxController::class, 'destroy']);
-    Route::get('/search/{value}', [InvoiceItemTaxController::class, 'search']);
-});
+Route::prefix('invoice-item-api')
+    ->name('invoice-item-api.')
+    ->group(function () {
+        Route::get('/', [InvoiceItemController::class, 'index'])->name('index');
+        Route::post('/', [InvoiceItemController::class, 'store'])->name('store');
+        Route::get('/{id}', [InvoiceItemController::class, 'show'])->name('show');
+        Route::put('/{id}', [InvoiceItemController::class, 'update'])->name('update');
+        Route::delete('/{id}', [InvoiceItemController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [InvoiceItemController::class, 'search'])->name('search');
+    });
 
-Route::prefix('payment-api')->group(function () {
-    Route::get('/', [PaymentController::class, 'index']);
-    Route::post('/', [PaymentController::class, 'store']);
-    Route::get('/{id}', [PaymentController::class, 'show']);
-    Route::delete('/{id}', [PaymentController::class, 'destroy']);
-    Route::get('/search/{value}', [PaymentController::class, 'search']);
-});
+Route::prefix('invoice-item-tax-api')
+    ->name('invoice-item-tax-api.')
+    ->group(function () {
+        Route::get('/', [InvoiceItemTaxController::class, 'index'])->name('index');
+        Route::post('/', [InvoiceItemTaxController::class, 'store'])->name('store');
+        Route::get('/{id}', [InvoiceItemTaxController::class, 'show'])->name('show');
+        Route::put('/{id}', [InvoiceItemTaxController::class, 'update'])->name('update');
+        Route::delete('/{id}', [InvoiceItemTaxController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [InvoiceItemTaxController::class, 'search'])->name('search');
+    });
 
-Route::prefix('expense-api')->group(function () {
-    Route::get('/', [ExpenseController::class, 'index']);
-    Route::post('/', [ExpenseController::class, 'store']);
-    Route::get('/{id}', [ExpenseController::class, 'show']);
-    Route::put('/{id}', [ExpenseController::class, 'update']);
-    Route::delete('/{id}', [ExpenseController::class, 'destroy']);
-    Route::get('/search/{value}', [ExpenseController::class, 'search']);
-});
+Route::prefix('payment-api')
+    ->name('payment-api.')
+    ->group(function () {
+        Route::get('/', [PaymentController::class, 'index'])->name('index');
+        Route::post('/', [PaymentController::class, 'store'])->name('store');
+        Route::get('/{id}', [PaymentController::class, 'show'])->name('show');
+        Route::delete('/{id}', [PaymentController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [PaymentController::class, 'search'])->name('search');
+    });
 
-Route::prefix('sales-attendance-api')->group(function () {
-    Route::get('/', [SalesAttendanceController::class, 'index']);
-    Route::post('/', [SalesAttendanceController::class, 'store']);
-    Route::get('/{id}', [SalesAttendanceController::class, 'show']);
-    Route::put('/{id}', [SalesAttendanceController::class, 'update']);
-});
+Route::prefix('expense-api')
+    ->name('expense-api.')
+    ->group(function () {
+        Route::get('/', [ExpenseController::class, 'index'])->name('index');
+        Route::post('/', [ExpenseController::class, 'store'])->name('store');
+        Route::get('/{id}', [ExpenseController::class, 'show'])->name('show');
+        Route::put('/{id}', [ExpenseController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ExpenseController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [ExpenseController::class, 'search'])->name('search');
+    });
 
-Route::prefix('user-api')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/', [UserController::class, 'store']);
-    Route::get('/{id}', [UserController::class, 'show']);
-    Route::put('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'destroy']);
-    Route::get('/search/{value}', [UserController::class, 'search']);
-});
+Route::prefix('sales-attendance-api')
+    ->name('sales-attendance-api.')
+    ->group(function () {
+        Route::get('/', [SalesAttendanceController::class, 'index'])->name('index');
+        Route::post('/', [SalesAttendanceController::class, 'store'])->name('store');
+        Route::get('/{id}', [SalesAttendanceController::class, 'show'])->name('show');
+        Route::put('/{id}', [SalesAttendanceController::class, 'update'])->name('update');
+    });
+
+Route::prefix('user-api')
+    ->name('user-api.')
+    ->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('/{id}', [UserController::class, 'show'])->name('show');
+        Route::put('/{id}', [UserController::class, 'update'])->name('update');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
+        Route::get('/search/{value}', [UserController::class, 'search'])->name('search');
+    });
 
 Route::get('/user', function (Request $request) {
     return $request->user();
