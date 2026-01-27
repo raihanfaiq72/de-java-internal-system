32d1480832030c5df0b4636ad8a32d1c73bb3429
Emerio Kevin Aryaputra <emerio.kevin@gmail.com>
2026-01-27 22:25:35 +0700

    fix(menu): regroup menu into parent and children

--- config/menu.php
+++ config/menu.php
@@ -89,14 +89,38 @@ return [
     [
         'type' => 'item',
         'title' => 'Penjualan',
-        'route' => 'sales',
         'icon' => 'iconoir-coins',
+        'id' => 'sidebarSales',
+        'submenu' => [
+            [
+                'title' => 'Invoice Penjualan',
+                'route' => 'sales',
+                'icon' => 'iconoir-coins',
+            ],
+            [
+                'title' => 'Kuitansi Penjualan',
+                'route' => 'sales.receipt',
+                'icon' => 'iconoir-hand-contactless',
+            ],
+        ]
     ],
     [
         'type' => 'item',
         'title' => 'Pembelian',
-        'route' => 'purchase',
         'icon' => 'iconoir-shop',
+        'id' => 'sidebarPurchase',
+        'submenu' => [
+            [
+                'title' => 'Invoice Pembelian',
+                'route' => 'purchase',
+                'icon' => 'iconoir-shop',
+            ],
+            [
+                'title' => 'Kuitansi Pembelian',
+                'route' => 'purchase.receipt',
+                'icon' => 'iconoir-delivery-truck',
+            ],
+        ]
     ],
     [
         'type' => 'item',
@@ -123,87 +147,70 @@ return [
         ],
     ],
     [
-        'type' => 'item',
-        'title' => 'Finance (Kuitansi)',
-        'icon' => 'iconoir-shop-window',
-        'id' => 'sidePO',
-        'submenu' => [
-            [
-                'title' => 'Kuitansi Penjualan',
-                'route' => 'sales.receipt',
-                'icon' => 'iconoir-hand-contactless',
-            ],
-            [
-                'title' => 'Kuitansi Pembelian',
-                'route' => 'purchase.receipt',
-                'icon' => 'iconoir-delivery-truck',
-            ],
-        ],
-    ],
-    [
         'type' => 'label',
         'title' => 'Report',
     ],
     [
         'type' => 'item',
-        'title' => 'Laporan Penjualan',
-        'route' => 'report.sales',
+        'title' => 'Laporan',
         'icon' => 'iconoir-graph-up',
+        'id' => 'sidebarLaporan',
+        'submenu' => [
+            [
+                'title' => 'Laporan Penjualan',
+                'route' => 'report.sales',
+                'icon' => 'iconoir-graph-up',
+            ],
+            [
+                'title' => 'Laporan Pembelian',
+                'route' => 'report.purchase',
+                'icon' => 'iconoir-reports',
+            ],
+            [
+                'title' => 'Laporan Stock',
+                'route' => 'report.stock',
+                'icon' => 'iconoir-archive',
+            ],
+            [
+
+                'title' => 'Neraca Keuangan',
+                'route' => 'report.balance-sheet',
+                'icon' => 'iconoir-table-rows',
+            ],
+            [
+
+                'title' => 'Manajemen COA',
+                'route' => 'report.coa-management',
+                'icon' => 'iconoir-list-select',
+            ],
+            [
+
+                'title' => 'Laba Rugi',
+                'route' => 'report.profit-loss',
+                'icon' => 'iconoir-calculator',
+            ],
+            [
+
+                'title' => 'Umur Piutang (AR Aging)',
+                'route' => 'report.ar-aging',
+                'icon' => 'iconoir-calendar',
+            ],
+            [
+
+                'title' => 'Buku Besar',
+                'route' => 'report.general-ledger',
+                'icon' => 'iconoir-book',
+            ],
+            [
+
+                'title' => 'Simulasi Harga Average',
+                'route' => '#',
+                'icon' => 'iconoir-info-circle',
+            ],
+        ]
+
     ],
-    [
-        'type' => 'item',
-        'title' => 'Laporan Pembelian',
-        'route' => 'report.purchase',
-        'icon' => 'iconoir-reports',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Laporan Stock',
-        'route' => 'report.stock',
-        'icon' => 'iconoir-archive',
-    ],
-    [
-        'type' => 'label',
-        'title' => '<small class="text-uppercase text-muted fw-bold" style="font-size: 10px; letter-spacing: 0.5px;">Laporan Keuangan</small>',
-        'is_html' => true,
-        'class' => 'mt-3',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Neraca Keuangan',
-        'route' => 'report.balance-sheet',
-        'icon' => 'iconoir-table-rows',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Manajemen COA',
-        'route' => 'report.coa-management',
-        'icon' => 'iconoir-list-select',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Laba Rugi',
-        'route' => 'report.profit-loss',
-        'icon' => 'iconoir-calculator',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Umur Piutang (AR Aging)',
-        'route' => 'report.ar-aging',
-        'icon' => 'iconoir-calendar',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Buku Besar',
-        'route' => 'report.general-ledger',
-        'icon' => 'iconoir-book',
-    ],
-    [
-        'type' => 'item',
-        'title' => 'Simulasi Harga Average',
-        'route' => '#',
-        'icon' => 'iconoir-info-circle',
-    ],
+
     [
         'type' => 'label',
         'title' => 'Setting',
